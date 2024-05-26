b2a8ffde22082bbcc3f30ca229404f636e4de3d8
bourbakiii <gabisov.basis@gmail.com>
2024-05-26 23:50:18 +0300

    --TASKS 10-15--

---
+++ main.go
@@ -0,0 +1,21 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"golang/task"
+)
+
+func main() {
+	task.Task10()
+	fmt.Println("---11---------------------------")
+	task.Task11()
+	fmt.Println("---12---------------------------")
+	task.Task12()
+	fmt.Println("---13---------------------------")
+	task.Task13()
+	fmt.Println("---14---------------------------")
+	task.Task14()
+	fmt.Println("---15---------------------------")
+	task.Task15()
+}
